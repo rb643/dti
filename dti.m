8ff30e36936df0719a52cd7fdfb4a5563ffe26de
rb643 <rb643@cam.ac.uk>
2016-08-16 15:20:06 +0100

    Update dti.m

--- dti.m
+++ dti.m
@@ -53,7 +53,6 @@ parse(p,varargin{:});
 nRand = p.Results.nRand; metric = p.Results.metric; regionDescriptions = p.Results.regionDescriptions; PlotAverage = p.Results.PlotAverage; PlotAll = p.Results.PlotAll; prev = p.Results.prev; groups = p.Results.groups; connectivity = p.Results.Matrix;
 
 %% And here we go with some actual analysis
-% Remove subjects with missing clinical data: CAT118, CAT120, CAT122
 nSubjects = size(connectivity,4);
 % Extract NOS matrix for every subject and remove singleton dimensions
 NOS_w = squeeze(connectivity(:,:,metric,:));
@@ -61,22 +60,11 @@ NOS_w = squeeze(connectivity(:,:,metric,:));
 NOS_b = double(NOS_w > 0);
 
 %% Inserting group membership and demographic and cognitive information
-Group0 = find(groups == 0);
-Group1 = find(groups == 1);
-Group2 = find(groups == 2);
-Group3 = find(groups == 3);
-Group4 = find(groups == 4);
-
-% Demographics and cognitive variables %% PROBABLY MOVE THIS TO A POSSIBLE
-% INPUT VARIABLE!
-Age = [75	78	74	69	81	78	78	75	67	71	74	87	79	81	81	71	73	73	78	76	79	64	75	71	69	69	73	81	79	70	66	78	84	79	81	82	81	79	81	83	72	63	62	74	70	78	79	69	77	62	87	80	78	78	77	76	65	84	73	88	79	80	81	80	77	87	71	89	78	71	80	76	70	62	73	80	78	70	75	66	80	75	79	89	81	88	88	67	66	74	73	77	76	80]';
-Gender = [1	1	2	2	2	1	2	1	1	2	2	2	1	1	2	1	2	1	1	1	2	1	1	1	1	1	1	1	1	1	1	1	1	1	1	2	2	1	1	1	1	1	1	1	1	1	1	1	1	1	1	1	2	1	1	1	1	1	1	2	1	1	2	1	1	1	1	1	1	1	1	1	1	1	1	1	1	1	1	1	2	2	1	2	2	1	1	1	1	1	1	2	1	1]';
-MMSE = [30	20	28	30	28	23	30	25	28	28	21	15	28	29	28	27	30	27	20	28	18	16	21	21	21	26	26	12	25	28	21	29	28	28	30	18	25	29	29	30	23	26	19	26	22	27	19	22	25	23	15	17	23	24	22	25	23	22	29	22	19	25	28	26	23	17	28	22	24	23	18	23	29	30	23	30	30	29	30	26	19	18	28	19	19	25	23	20	25	26	25	16	19	16]';
+Group0 = find(groups == 0); Group1 = find(groups == 1); Group2 = find(groups == 2); Group3 = find(groups == 3); Group4 = find(groups == 4);
 
 %% Quality control
 % Compute prevalence matrix of entire sample which will be used later.
 P = mean(connectivity(:,:,metric,:) > 0, 4);
-
 % Create a vector of average NOS over all connections of a subject
 for i = 1:nSubjects
     NOS = connectivity(:,:,metric,i);
@@ -92,12 +80,11 @@ for i = 1:nSubjects
     W = connectivity(:,:,1,i);
     B = double(W > 0);
     C = B + eye(size(B));
-    AvgPrevMissingCon(i,1) = mean(P(C == 0)); %why need 1 here and not for AvgPrevCon?
+    AvgPrevMissingCon(i,1) = mean(P(C == 0));
 end
 
 % Store all QC variables into a QC matrix
 matrix_QC = [average_NOS average_FA AvgPrevCon AvgPrevMissingCon];
-
 % Plot output for QC
 if PlotAverage == 1
     figure;
@@ -105,50 +92,6 @@ if PlotAverage == 1
     subplot(2,2,2); boxplot(matrix_QC(:,2),groups,'colorgroup',groups);title('FA');
     subplot(2,2,3); boxplot(matrix_QC(:,3),groups,'colorgroup',groups);title('Avg Prev of Connections');
     subplot(2,2,4); boxplot(matrix_QC(:,4),groups,'colorgroup',groups);title('Avg Prev of Missing Connections');
-end
-
-%% Global metrics
-% Create prevalence matrix of the sample
-P_Sample = mean(connectivity(:,:,metric,:) > 0, 4);
-A_Sample = double(P>=0.6);
-
-P_0= mean(connectivity(:,:,metric,Group0) > 0, 4);
-A_0 = double(P_0>=0.5);
-
-P_1 = mean(connectivity(:,:,metric,Group1) > 0, 4);
-A_1 = double(P_1>=0.5);
-
-P_2= mean(connectivity(:,:,metric,Group2) > 0, 4);
-A_2 = double(P_2>=0.5);
-
-P_3 = mean(connectivity(:,:,metric,Group3) > 0, 4);
-A_3= double(P_3>=0.5);
-
-if PlotAverage == 1
-    figure;
-    subplot(2,2,1);
-    imagesc(A_0); title('0');
-    subplot(2,2,2);
-    imagesc(A_1); title('1');
-    subplot(2,2,3);
-    imagesc(A_2); title('2');
-    subplot(2,2,4);
-    imagesc(A_3); title('3');
-end
-
-% Create degree distribution
-degrees_0 = sum(A_0,2); degrees_1 = sum(A_1,2); degrees_2 = sum(A_2,2); degrees_3 = sum(A_3,2);
-
-if PlotAverage == 1
-    figure;
-    subplot(2,2,1);
-    hist(degrees_0); title('0');  xlabel('deg of prevalence > 0.4')
-    subplot(2,2,2);
-    hist(degrees_1); title('1');  xlabel('deg of prevalence > 0.4')
-    subplot(2,2,3);
-    hist(degrees_2); title('2');  xlabel('deg of prevalence > 0.4')
-    subplot(2,2,4);
-    hist(degrees_3); title('3');  xlabel('deg of prevalence > 0.4')
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%% Unweighted NOS %%%%%%%%%%%%%%%%%%%%%%%%%%%%
